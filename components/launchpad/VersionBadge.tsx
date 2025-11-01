7bb3f2848bbeb25c696129d37b77207f1390eca9
Oliver Warren <yenhoang.it@ecombest.co>
2025-11-02 01:55:10 +0700

    Update VersionBadge to display PWA version format
    - Modify VersionBadge component to conditionally display the app version with a 'P_' prefix when running as a PWA, enhancing clarity for users regarding the app's context.

--- components/launchpad/VersionBadge.tsx
+++ components/launchpad/VersionBadge.tsx
@@ -1,9 +1,12 @@
+import { useIsPwa } from "@/hooks/useIsPwa";
 import { APP_VERSION } from "@lib/version";
 
 export function VersionBadge(): JSX.Element {
+  const isPwa = useIsPwa();
+
   return (
     <button className="pointer-events-none w-fit mx-auto my-2 z-30 rounded-full bg-slate-950/20 px-3 py-1 text-[10px] font-medium uppercase tracking-[0.24em] text-slate-200/25 backdrop-blur-md">
-      v{APP_VERSION}
+      {isPwa ? 'P_' + APP_VERSION : 'v' +APP_VERSION}
     </button>
   );
 }
